836105c8fed73e00c2f6628268fbed89a5ccc11a
Jared Farkas and Riley Benson <jared@>
2019-01-15 15:22:52 -0800

    put wells into two columns

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,15 @@
 $(document).ready(function() {
   $("button#bark").click(function() {
     $("ul#dog").append("<li>bark!</li>");
+    $("li").click(function() {
+      $(this).remove();
+    });
   });
   $("button#meow").click(function() {
     $("ul#cat").append("<li>meow!</li>");
+    $("li").click(function() {
+      $(this).remove();
+    });
   });
 
-  $("li").click(function() {
-    $(this).remove();
-  });
 });
